1b3684180ca85d0832ac91f29eaa96ba6eb4ab49
yeruham <y0583207549@gmail.com>
2025-07-23 10:06:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -51,4 +51,4 @@ if __name__ == "__main__":
     df = model.df
     model.run_trainer()
     model.run_validator()
-    uvicorn.run(app, host= '127.0.0.1', port= 8001)
+    uvicorn.run(app, host= '0.0.0.0', port= 8001)
